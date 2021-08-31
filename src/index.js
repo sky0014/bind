02da0e4b6f05867db087c9c4bb355700ec158c76
wangxingwei <wangxingwei@douyu.tv>
2021-08-31 19:20:42 +0800

    release 1.0.3

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 /**
  * 遍历对象，绑定全部函数
- * @param {*} target 要绑定到的目标
+ * @param {*} target 要绑定的目标
  * @returns {void}
  */
-export function bindAll(target) {
-    const that = target;
-    const bindFunctions = {};
-    while (target && target !== Object.prototype) {
-      Object.getOwnPropertyNames(target).forEach(name => {
-        const config = Object.getOwnPropertyDescriptor(target, name);
-        if (config) {
-          if (config.get || config.set) {
-            return;
-          }
-  
-          if (typeof that[name] === "function") {
-            // 去重存储函数名
-            bindFunctions[name] = true;
-          }
+ export function bindAll(target) {
+  const that = target;
+  const bindFunctions = {};
+  while (target && target !== Object.prototype) {
+    Object.getOwnPropertyNames(target).forEach(name => {
+      const config = Object.getOwnPropertyDescriptor(target, name);
+      if (config) {
+        if (config.get || config.set) {
+          return;
         }
-      });
-      target = Object.getPrototypeOf(target);
-    }
-    Object.keys(bindFunctions).forEach(name => {
-      that[name] = that[name].bind(that);
+
+        if (name !== "constructor" && typeof that[name] === "function") {
+          // 去重存储函数名
+          bindFunctions[name] = true;
+        }
+      }
     });
+    target = Object.getPrototypeOf(target);
   }
-  
+  Object.keys(bindFunctions).forEach(name => {
+    that[name] = that[name].bind(that);
+  });
+}
